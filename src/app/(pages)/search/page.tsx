f8e8a17b8a6529d3c90b4c9504e2b5164187c664
Le Van Dai <levandai831@gmail.com>
2025-03-16 18:39:03 +0700

    Project 5

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import Section_1 from "./Section-1";
 
 export default function SearchPage() {
     return (
       <>
+      <Suspense>
         <Section_1 />
+      </Suspense>
       </>
     );
   }
